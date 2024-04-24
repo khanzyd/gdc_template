90b6fb8174355c9120a94b2c746f6ee0d0e004f6
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-04-24 23:13:12 +0530

    a

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Home_Carousel from "@/components/Home_Carousel";
+
+export default function Home() {
+
+  return (
+    <div className="relative">
+      <Home_Carousel/>
+    </div>
+  );
+}
