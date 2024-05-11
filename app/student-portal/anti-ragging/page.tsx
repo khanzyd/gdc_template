9d9ae1aa8db34ca8dac9f74e592721a6576c2a96
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-05-11 22:07:28 +0530

    updated quick links

--- app/student-portal/anti-ragging/page.tsx
+++ app/student-portal/anti-ragging/page.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 export default function Page(){
-    return <div className="px-44 py-10">
+    return <div className="px-48 py-10">
         <h1 className="text-4xl mb-10">Anti Ragging</h1>
         <p>For the information on Anti-ragging please visit the website:</p>
-        <a href="https://www.ugc.ac.in/UGC_Regulations_Student_Centric.aspx">
+        <a href="https://www.ugc.ac.in/UGC_Regulations_Student_Centric.aspx"
+            className="text-[--primary-color] underline inline-block mb-10"
+        >
             https://www.ugc.ac.in/UGC_Regulations_Student_Centric.aspx
         </a>
 
-        <p>For Online student and Parent Undertaking visit website:</p>
-        <p>www.antiragging.in OR www.amanmovement.org</p>
+        <p>
+            For Online student and Parent Undertaking visit website: www.antiragging.in OR www.amanmovement.org
+        </p>
     </div>
 }
