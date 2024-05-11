526529569b969626ec89e82db83885a906edd65b
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-05-11 19:04:22 +0530

    updated navbar

--- app/recruitment-cell/page.tsx
+++ app/recruitment-cell/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function Page(){
+export default function Page() {
     return <>
-        recruitment cell
+        STAFF RECRUITMENT COMMITTEE
     </>
 }
