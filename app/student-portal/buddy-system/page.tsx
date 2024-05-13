4dd045541c45c5101a25498159215eee0324a9c8
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-05-13 18:24:41 +0530

    updated student portal

--- app/student-portal/buddy-system/page.tsx
+++ app/student-portal/buddy-system/page.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function Page(){
-    return <></>
+export default function Page() {
+    return <div className="sm:px-48 px-4 py-10">
+        <h1 className="text-4xl mb-4">Buddy System</h1>
+        <p className="text-justify pt-4">
+            The GDCH Buddy System program is an initiative to help connect GDCH students who would like to meet friends or establish new connections (whether you're taking in-person or virtual classes). We know college can be a big adjustment - whether you're transitioning from high school, transferring from another college, currently a GDCH student or coming back to college after some time away - the Buddy System connects you with another GDCH student!
+        </p>
+        <p className="text-justify pt-4">
+            The Buddy System accepts new Buddies at the start of the Fall and Spring semesters. Participants in the program will receive some free GDCH give-a-ways, invitations to exclusive Buddy System events, and will receive weekly discussion topics to help facilitate conversation with your buddy.
+        </p>
+        <p className="text-justify pt-4">
+            You can carry on buddy system correspondence via email or move to another preferred mode of communication. This is a great opportunity to make new friends, learn more about how other students on campus are doing, and establish more connections on campus.
+        </p>
+    </div>
 }
