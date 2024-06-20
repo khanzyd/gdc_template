a228cd83e4846dc365488df26bdb7fe9b80dd640
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-06-20 21:36:40 +0530

    updated conservative dentistry departments page

--- components/department/Faculty.tsx
+++ components/department/Faculty.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 function Faculty(props: {profilePicSrc: string, cvSrc: string, facultyName: string, facultyDesignation:string}) {
     return (
-        <div className='max-w-[200px] shadow-md outline outline-gray-300 outline-1 rounded flex flex-col justify-center items-center text-center'>
+        <div className='max-w-[200px] shadow-md outline outline-gray-300 outline-1 rounded flex flex-col items-center text-center'>
             <img
                 src={props.profilePicSrc}
                 alt='faculty'
-                width={200}
-                height={200}
+                className='w-[200px]'
             />
             <a className='hover:underline text-[var(--primary-color)]' href={props.cvSrc} type="application/pdf" target='_blank'>
                 <h1 className='text-sm font-medium p-4'>{props.facultyName}</h1>
