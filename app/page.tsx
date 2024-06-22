6a6bdf38c21d425790346c44d8d6dfc8d3fdf68e
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-06-22 17:53:55 +0530

    updated carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import Image from "next/image";
-import arr from "@/public/achievement_data";
+import arr from "@/app/achievement_data";
 import HomeCarousel from "@/components/HomeCarousel";
 import left_arrow from "@/public/left-arrow.svg";
 import right_arrow from "@/public/right-arrow.svg";
@@ -11,69 +13,92 @@ import CouncilMemberCard from "@/components/CouncilMemberCard";
 import NumberSays from "@/components/NumberSays";
 import TestimonialCard from "@/components/TestimonialCard";
 import Maintenance from "@/components/Maintenance";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    return (
-        <Maintenance></Maintenance>
-        // <div className="relative overflow-hidden">
-        //     <HomeCarousel />
-        //     <div className="flex mb-10 mt-24 justify-evenly h-fit py-10">
-        //         {arr.map((data) => {
-        //             return <Box svg={data.url} heading={data.heading} para={data.para} />;
-        //         })}
-        //     </div>
-        //     <div className=" relative">
-        //         <div className=" absolute overflow-hidden max-h-[30rem] -z-10 opacity-[0.17]  ">
-        //             <Image
-        //                 src={"/college4.jpg"}
-        //                 className="mx-4 faded-image "
-        //                 width={1920}
-        //                 height={880}
-        //                 alt=""
-        //             />
-        //         </div>
+    let collegeImages = [
+        "/home/Carousel1.jpg",
+        "/home/Carousel2.jpg",
+        "/home/Carousel3.jpg",
+        "/home/Carousel4.jpg",
+        "/home/Carousel5.jpg",
+        "/home/Carousel6.jpg",
+        "/home/Carousel7.jpg",
+        "/home/Carousel8.jpg",
+        "/home/Carousel9.jpg",
+        "/home/Carousel10.jpg",
+        "/home/Carousel11.jpg",
+        "/home/Carousel12.jpg",
+        "/home/Carousel13.jpg",
+        "/home/Carousel14.jpg",
+        "/home/Carousel15.jpg",
+        "/home/Carousel16.jpg",
+        "/home/Carousel17.jpg",
+        "/home/Carousel18.jpg",
+        "/home/Carousel19.jpg",
+        "/home/Carousel20.jpg",
+    ];
 
-        //         <CollegeInfo />
-        //     </div>
-        //     <DeansMessage />
-        //     <LifeAtGDC />
-        //     <section className="flex flex-col items-center my-16">
-        //         <h1 className="text-[#82261B] text-2xl font-semibold mb-16 underline">
-        //             MEMBERS OF COLLEGE COUNCIL
-        //         </h1>
-        //         <div className="flex flex-wrap gap-8 px-32 justify-center">
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //             <CouncilMemberCard></CouncilMemberCard>
-        //         </div>
-        //     </section>
-        //     <section className="bg-[#a2463B] text-white flex flex-col items-center my-16">
-        //         <h1 className="underline text-2xl font-semibold pt-16 mb-16">
-        //             THE NUMBERS SAYS IT ALL
-        //         </h1>
-        //         <div className="flex gap-16 px-32 pb-16">
-        //             <NumberSays></NumberSays>
-        //             <div className="h-fill w-[2px] bg-white"></div>
-        //             <NumberSays></NumberSays>
-        //             <div className="h-fill w-[2px] bg-white"></div>
-        //             <NumberSays></NumberSays>
-        //             <div className="h-fill w-[2px] bg-white"></div>
-        //             <NumberSays></NumberSays>
-        //         </div>
-        //     </section>
-        //     <section className="flex gap-16 justify-center items-center my-16 px-32 py-32">
-        //         <img src={left_arrow.src}></img>
-        //         <TestimonialCard></TestimonialCard>
-        //         <TestimonialCard></TestimonialCard>
-        //         <img src={right_arrow.src}></img>
-        //     </section>
-        // </div>
+    return (
+        <div className="relative overflow-hidden">
+            <HomeCarousel imgSrcs={collegeImages}/>
+            {/* <div className="flex mb-10 mt-24 justify-evenly h-fit py-10">
+                 {arr.map((data) => {
+                     return <Box svg={data.url} heading={data.heading} para={data.para} />;
+                 })}
+             </div>
+             <div className=" relative">
+                 <div className=" absolute overflow-hidden max-h-[30rem] -z-10 opacity-[0.17]  ">
+                     <Image
+                         src={"/college4.jpg"}
+                         className="mx-4 faded-image "
+                         width={1920}
+                         height={880}
+                         alt=""
+                     />
+                 </div>
+
+                 <CollegeInfo />
+             </div>
+             <DeansMessage />
+             <LifeAtGDC />
+             <section className="flex flex-col items-center my-16">
+                 <h1 className="text-[#82261B] text-2xl font-semibold mb-16 underline">
+                     MEMBERS OF COLLEGE COUNCIL
+                 </h1>
+                 <div className="flex flex-wrap gap-8 px-32 justify-center">
+                     <CouncilMemberCard></CouncilMemberCard>
+                     <CouncilMemberCard></CouncilMemberCard>
+                     <CouncilMemberCard></CouncilMemberCard>
+                     <CouncilMemberCard></CouncilMemberCard>
+                     <CouncilMemberCard></CouncilMemberCard>
+                     <CouncilMemberCard></CouncilMemberCard>
+                     <CouncilMemberCard></CouncilMemberCard>
+                     <CouncilMemberCard></CouncilMemberCard>
+                     <CouncilMemberCard></CouncilMemberCard>
+                     <CouncilMemberCard></CouncilMemberCard>
+                 </div>
+             </section>
+             <section className="bg-[#a2463B] text-white flex flex-col items-center my-16">
+                 <h1 className="underline text-2xl font-semibold pt-16 mb-16">
+                     THE NUMBERS SAYS IT ALL
+                 </h1>
+                 <div className="flex gap-16 px-32 pb-16">
+                     <NumberSays></NumberSays>
+                     <div className="h-fill w-[2px] bg-white"></div>
+                     <NumberSays></NumberSays>
+                     <div className="h-fill w-[2px] bg-white"></div>
+                     <NumberSays></NumberSays>
+                     <div className="h-fill w-[2px] bg-white"></div>
+                     <NumberSays></NumberSays>
+                 </div>
+             </section>
+             <section className="flex gap-16 justify-center items-center my-16 px-32 py-32">
+                 <img src={left_arrow.src}></img>
+                 <TestimonialCard></TestimonialCard>
+                 <TestimonialCard></TestimonialCard>
+                 <img src={right_arrow.src}></img>
+             </section> */}
+        </div>
     );
 }
