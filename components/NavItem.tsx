97690b2a4eb66e6ab333f904f7d7bc0ab3e01b55
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-06-29 15:12:48 +0530

    added about gdc and deans message

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react";
 
 export default function NavItem({href, children}: {href: string, children: string | ReactElement}){
     return <div className="w-full lg:w-fit">
-        <a href={href} className="block hover:bg-[var(--light-primary-color)] px-4 py-2 duration-200">
+        <a href={href} className="block hover:bg-[var(--light-primary-color)] px-4 py-4 duration-200">
             {children}
         </a>
     </div>
